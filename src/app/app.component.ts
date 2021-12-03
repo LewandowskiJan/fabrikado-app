f6325153a58d0bc66adf23a4b0a2a633c0f8d0a6
Jan Lewandowski <lewandowski.jan93@gmail.com>
2021-12-03 11:31:40 +0100

    chore: config eslint for accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fabrikado-app';
+
+  public constructor(private titleService: Title) {}
+
+  public setTitle(newTitle: string): void {
+    this.titleService.setTitle(newTitle);
+  }
+
+  public onClick(): void {
+    console.log('clicked');
+  }
 }
