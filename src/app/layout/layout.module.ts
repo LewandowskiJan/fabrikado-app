38c5025ea24f3f5dcf616477c50e60a630d82295
Jan Lewandowski <lewandowski.jan93@gmail.com>
2021-12-05 13:07:17 +0100

    feat:  - buttons added  - layouts added

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ButtonComponent } from './components/button/button.component';
+import { MenuButtonComponent } from './components/menu-button/menu-button.component';
+import { PlanetButtonComponent } from './components/planet-button/planet-button.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { LayoutComponent } from './containers/layout/layout.component';
 import { PlanetContainerComponent } from './containers/planet-container/planet-container.component';
 
 @NgModule({
-  declarations: [PlanetContainerComponent],
+  declarations: [
+    PlanetContainerComponent,
+    ButtonComponent,
+    DashboardComponent,
+    PlanetButtonComponent,
+    LayoutComponent,
+    MenuButtonComponent,
+  ],
   imports: [CommonModule],
-  exports: [PlanetContainerComponent],
+  exports: [
+    PlanetContainerComponent,
+    ButtonComponent,
+    DashboardComponent,
+    PlanetButtonComponent,
+    LayoutComponent,
+    MenuButtonComponent,
+  ],
 })
 export class LayoutModule {}
