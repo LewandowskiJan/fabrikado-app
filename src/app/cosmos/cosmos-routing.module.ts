0df62866d73ffcbe5d575a149cdf2e761f27182b
kanademruuu@gmail.com <kanademruuu@gmail.com>
2021-12-05 13:12:12 +0100

    2nd push

--- src/app/cosmos/cosmos-routing.module.ts
+++ src/app/cosmos/cosmos-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BuildingsComponent } from './components/buildings/buildings.component';
+import { MarketComponent } from './components/market/market.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { CosmosComponent } from './containers/cosmos/cosmos.component';
 import { PlanetComponent } from './containers/planet/planet.component';
@@ -18,6 +19,15 @@ const routes: Routes = [
           { path: '', redirectTo: 'overview', pathMatch: 'full' },
           { path: 'overview', component: OverviewComponent },
           { path: 'buildings', component: BuildingsComponent },
+          { path: 'market', component: MarketComponent },
+          { path: 'research', component: MarketComponent },
+          { path: 'shipyard', component: MarketComponent },
+          { path: 'defence', component: MarketComponent },
+          { path: 'fleet', component: MarketComponent },
+          { path: 'galaxy', component: MarketComponent },
+          { path: 'alliance', component: MarketComponent },
+          { path: 'staff', component: MarketComponent },
+          { path: 'shop', component: MarketComponent },
         ],
       },
     ],
--- src/app/cosmos/cosmos.module.ts
+++ src/app/cosmos/cosmos.module.ts
@@ -2,8 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LayoutModule } from './../layout/layout.module';
+import { AllianceComponent } from './components/alliance/alliance.component';
 import { BuildingsComponent } from './components/buildings/buildings.component';
+import { DefenceComponent } from './components/defence/defence.component';
+import { FleetComponent } from './components/fleet/fleet.component';
+import { GalaxyComponent } from './components/galaxy/galaxy.component';
+import { MarketComponent } from './components/market/market.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { ResearchComponent } from './components/research/research.component';
+import { ShipyardComponent } from './components/shipyard/shipyard.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { StaffComponent } from './components/staff/staff.component';
 import { CosmosComponent } from './containers/cosmos/cosmos.component';
 import { PlanetComponent } from './containers/planet/planet.component';
 import { CosmosRoutingModule } from './cosmos-routing.module';
@@ -14,6 +23,15 @@ import { CosmosRoutingModule } from './cosmos-routing.module';
     PlanetComponent,
     OverviewComponent,
     BuildingsComponent,
+    MarketComponent,
+    ResearchComponent,
+    ShipyardComponent,
+    DefenceComponent,
+    FleetComponent,
+    GalaxyComponent,
+    AllianceComponent,
+    StaffComponent,
+    ShopComponent,
   ],
   imports: [CommonModule, CosmosRoutingModule, LayoutModule],
 })
