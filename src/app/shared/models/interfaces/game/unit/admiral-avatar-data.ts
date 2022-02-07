747c1f54d823c3a78ba3f967620a51a3f032c37f
kanademruuu@gmail.com <kanademruuu@gmail.com>
2022-02-07 18:38:26 +0100

    admiral-avatar-tooltip

---
+++ src/app/shared/models/interfaces/game/unit/admiral-avatar-data.ts
@@ -0,0 +1,12 @@
+export interface AdmiralAvatarData {
+  rarity: string;
+  name: string;
+  admiralAvatar: string;
+  buffs: {
+    flightSpeedIncrease: number; // 0/100 %
+    dmgIncrease: number; // 0/100 %
+    defIncrease: number; // 0/100 %
+    antiRadar: string
+    other: string;
+    other2: string;
+}}
